ed14e531c91bb457bb5d047a519989ceeedc8519
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-06 19:14:20 +0530

    [Codeforces] 4A---Watermelon - Accepted (ID: 385893679)

---
+++ Codeforces/4A---Watermelon/385893679_Attempt_27_AC.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int userInput;
+    cin >> userInput;
+    
+    if(userInput <= 2 || userInput%2 != 0)
+    {
+        cout << "NO";
+    }
+    
+    if(userInput > 2 && userInput%2 == 0)
+    {
+        cout << "YES";
+    }
+    return 0;
+}
