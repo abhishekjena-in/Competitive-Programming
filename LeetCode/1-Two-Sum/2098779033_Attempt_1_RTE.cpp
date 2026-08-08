5ca39f9731eb5ae39dbbe17490187c4e85a360cd
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-08 12:34:49 +0530

    [LeetCode] 1-Two-Sum - Runtime Error (ID: 2098779033)

---
+++ LeetCode/1-Two-Sum/2098779033_Attempt_1_RTE.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        int i=0;
+        int j=1;
+        while(i <= nums.size())
+        {
+            if(nums.at(i) + nums.at(j) == target)
+            {
+                result.push_back(i);
+                result.push_back(j);
+                break;
+            }
+            i++;
+            j++;
+        }
+        return result;
+    }
+};
