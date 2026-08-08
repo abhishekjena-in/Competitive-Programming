e9c699d0a2b8c5e9f498607f07154a5e4818284c
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-08 12:42:28 +0530

    [LeetCode] 1-Two-Sum - Accepted (ID: 2098786573)

---
+++ LeetCode/1-Two-Sum/2098786573_Attempt_3_AC.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            for(int j=nums.size()-1; j>i; j--)
+            {
+                if(nums.at(i) + nums.at(j) == target)
+                {
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
