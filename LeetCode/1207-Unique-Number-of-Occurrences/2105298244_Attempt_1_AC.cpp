fd7d774b1f63c6e30c753298173afea3b8076153
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-13 15:55:03 +0530

    [LeetCode] 1207-Unique-Number-of-Occurrences - Accepted (ID: 2105298244)

---
+++ LeetCode/1207-Unique-Number-of-Occurrences/2105298244_Attempt_1_AC.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> m;
+        for(int i=0; i<arr.size(); i++)
+        {
+            m[arr[i]]++;
+        }
+
+        unordered_set<int> s;
+        for(auto p : m)
+        {
+            if(s.find(p.second) == s.end())
+            {
+                s.insert(p.second);
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
