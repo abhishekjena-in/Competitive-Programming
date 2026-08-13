b47f8ecb4ada0b44222211a622a52770e8a01b7c
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-13 06:41:38 +0530

    [GeeksforGeeks] Second-Largest - Wrong Answer (ID: gfg_1786583495773)

---
+++ GeeksforGeeks/problems/second-largest3735/gfg_1786583495773_Attempt_1_WA.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int lar= Integer.MIN_VALUE;
+        int secondLar= Integer.MIN_VALUE;
+        
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] > lar)
+            {
+                secondLar = lar;
+                lar= arr[i];
+            }
+            
+            if(arr[i]<lar && arr[i]>secondLar)
+            {
+                secondLar = arr[i];
+            }
+        }
+        
+        return secondLar;
+    }
+}
