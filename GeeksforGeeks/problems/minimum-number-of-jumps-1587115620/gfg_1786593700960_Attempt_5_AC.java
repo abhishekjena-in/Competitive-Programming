4e2e9040e540f97b4c848d19ea866a5dfcd3012f
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-13 09:31:46 +0530

    [GeeksforGeeks] Minimum-Jumps - Accepted (ID: gfg_1786593700960)

---
+++ GeeksforGeeks/problems/minimum-number-of-jumps-1587115620/gfg_1786593700960_Attempt_5_AC.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int search(int arr[], int x) {
+        int found = -1;
+        for(int i=0; i<arr.length; i++)
+        {
+            if(arr[i] == x)
+            {
+                found = i;
+                break;
+            }
+        }
+        return found;
+        
+    }
+}
