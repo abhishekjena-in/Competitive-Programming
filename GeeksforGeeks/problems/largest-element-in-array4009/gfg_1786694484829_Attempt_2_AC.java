d95d38def2ef6c3493f9a6d825f84bde3edc1b6e
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-14 13:31:30 +0530

    [GeeksforGeeks] Largest-in-Array - Accepted (ID: gfg_1786694484829)

---
+++ GeeksforGeeks/problems/largest-element-in-array4009/gfg_1786694484829_Attempt_2_AC.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largest(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= largest) {
+                largest = arr[i];
+            }
+        }
+
+        return largest;
+    }
+}
