8485d6fd1c294a3d942886c8b958b3218e6da225
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-15 07:20:40 +0530

    [CodeChef] World-Chess-Championship - Compilation Error (ID: cc_1786758633339)

---
+++ CodeChef/practice/course/strings-new/STRINGSP01/problems/WCC/cc_1786758633339_Attempt_3_CE.java
@@ -0,0 +1,35 @@
+int X = sc.nextInt();
+            String result = sc.nextLine();
+            
+            int countC = 0;
+            int countD = 0;
+            int countN = 0;
+            for(int j=0; j<result.length(); j++)
+            {
+                char lowerCh = result.charAt(j);
+                char ch = lowerCh.toUpperCase();
+                switch (ch)
+                {
+                    case 'C':
+                        countC++;
+                        break;
+                        
+                    case 'N':
+                        countN++;
+                        break;
+                        
+                    case 'D':
+                        countD++;
+                        
+                    default:
+                        break;
+                }
+            }
+            
+            if(countC == countN){
+                prize = 55 *  X;
+ThreadGroup keyword
+ClassNotFoundException keyword
+IndexOutOfBoundsException keyword
+ArrayIndexOutOfBoundsException keyword
+StringIndexOutOfBoundsException keyword
