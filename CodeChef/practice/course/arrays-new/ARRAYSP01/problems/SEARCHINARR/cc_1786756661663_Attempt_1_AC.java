f7c3bf48e48d64b7da50de7c8095acb7ab62b29f
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-15 06:47:51 +0530

    [CodeChef] Search-an-element-in-an-array - Accepted (ID: cc_1786756661663)

---
+++ CodeChef/practice/course/arrays-new/ARRAYSP01/problems/SEARCHINARR/cc_1786756661663_Attempt_1_AC.java
@@ -0,0 +1,20 @@
+public static String solve(int N, int X, int[] A) {
+        for(int i=0; i<A.length; i++)
+        {
+            if(A[i] == X)
+            {
+                return "YES";
+            }
+        }
+        
+        return "NO";
+}
+NoClassDefFoundError keyword
+NoSuchFieldError keyword
+NoSuchFieldException keyword
+NoSuchMethodError keyword
+NoSuchMethodException keyword
+UnknownError keyword
+NullPointerException keyword
+TypeNotPresentException keyword
+ClassNotFoundException keyword
