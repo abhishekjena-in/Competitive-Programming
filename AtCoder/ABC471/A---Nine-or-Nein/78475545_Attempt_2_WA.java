cbe55ff9bc985c0b9f8d991f9a2f428e9ba31c45
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-16 22:11:06 +0530

    [AtCoder] A---Nine-or-Nein - WA (ID: 78475545)

---
+++ AtCoder/ABC471/A---Nine-or-Nein/78475545_Attempt_2_WA.java
@@ -0,0 +1,20 @@
+public class Main{
+  public static void main(String[] args)
+  {
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    
+    if(
+      A+B == 9 ||
+      A-B == 9 ||
+      A*B == 9 ||
+      Math.ceil( (double)A/B) == 9){
+        System.out.println("Nine");
+      }
+    else{
+      System.out.println("Nein");
+    }
+  }
+}
