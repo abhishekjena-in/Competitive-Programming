0af21f2e9a890ee20f2260634e04bfffd9fe7d59
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-17 20:29:04 +0530

    [AtCoder] B---Survey-Tabulation - AC (ID: 78494462)

---
+++ AtCoder/ABC471/B---Survey-Tabulation/78494462_Attempt_3_AC.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    
+    HashMap<String, Integer> hm = new HashMap<>();
+    
+    int N = sc.nextInt();
+    int max = 0;
+    for(int i=0; i<N; i++)
+    {
+      String input = sc.next();
+      String s = input.toLowerCase();
+      
+    //   int curr = hm.getOrDefault(s, 1);
+    //   if(curr > max)
+    //   {
+    //     max = curr;
+    //   }
+
+        if(hm.get(s) == null)
+        {
+            hm.put(s, 1);
+            int curr = hm.get(s);
+            if(curr > max) {
+                max = curr;
+            }
+        }else{
+            hm.put(s, hm.get(s) + 1);
+            int curr = hm.get(s);
+            if(curr > max) {
+                max = curr;
+            }
+        }
+    }
+    
+    System.out.println(max);
+  }
+}
