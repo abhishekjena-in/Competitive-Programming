c6e3a1ec358aec555e8af470b8e9b8b97ce36ec7
Abhishek Jena <abhishekjena343@gmail.com>
2026-08-25 04:59:39 +0530

    [CodeKata] Ninja-Clan-Difference - Accepted (ID: ck_1787614175750)

---
+++ CodeKata/ninja-clan-difference/ck_1787614175750_Attempt_1_AC.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    
+    int diff = abs(A - B);
+    
+    cout << diff;
+    return 0;
+}
